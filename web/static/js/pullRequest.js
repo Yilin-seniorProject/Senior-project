372fa91b53f0f9131fbe022273538408cd1e05df
Willy Chen <willy100457@gmail.com>
2025-02-19 22:24:44 +0800

    [fix]counting problems & adjusting database

--- web/static/js/pullRequest.js
+++ web/static/js/pullRequest.js
@@ -34,7 +34,7 @@ export function imgRequest(markerId) {
                 document.getElementById("popup_img").src = data.image_path;
                 document.getElementById("drone_alt").innerHTML = "無人機拍攝高度： " + data.drone_info[2] + " 公尺";
                 // TODO 違規判斷式與記數
-                if (data.message) {
+                if (data.message[0]) {
                     document.getElementById("violation").innerHTML = "車輛是否違規： 是 Yes";
                 } else {
                     document.getElementById("violation").innerHTML = "車輛是否違規： 否 No";
